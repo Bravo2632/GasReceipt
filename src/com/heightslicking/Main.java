5fae7241bf798ab113312b422e576ff846e93953
Brandon Gibson <Brandon.gibson.2022@student.lhschools.org>
2020-02-28 13:54:42 -0500

    Gas Receipt Commit

---
+++ src/com/heightslicking/Main.java
@@ -0,0 +1,29 @@
+package com.heightslicking;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("_________________________");
+        System.out.println("|        Gibson         |");
+        System.out.println("|      Gas Station      |");
+        System.out.println("|960 Sara Ct            |");
+        System.out.println("|Pataskala, Ohio        |");
+        System.out.println("|  Date       Time      |");
+        System.out.println("| 1/10/2020   1:46 PM   |");
+        System.out.println("|                       |");
+        System.out.println("|Invoice# 1010101       |");
+        System.out.println("|Authorization 4****    |");
+        System.out.println("|                       |");
+        System.out.println("| Pump Product   $/G    |");
+        System.out.println("|  5    Gas     2.71    |");
+        System.out.println("|                       |");
+        System.out.println("|  Gallons     Total/$  |");
+        System.out.println("|     15        40.65   |");
+        System.out.println("|                       |");
+        System.out.println("|       Thank You       |");
+        System.out.println("|         Gibson        |");
+        System.out.println("|       Gas Station     |");
+        System.out.println("|_______________________|");
+	// write your code here
+    }
+}
